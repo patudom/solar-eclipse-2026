b4458cd8b28331d895b88ff1e6b65b91eba8e1fe
patudom <pudompra@cfa.harvard.edu>
2026-07-16 17:31:29 -0400

    Show '(slowed for totality)' in speed label while playing near totality
    playbackRate already clamps to 10x when forceRate is true (the app
    auto-slows fast playback as it approaches totality), but the speed
    label gave no indication why the rate had changed while playing --
    only the (paused) suffix existed, and only for the non-playing case.
    Verified via eclipsePrediction.maxTime: at Antiguita, Spain during
    totality with playing=true, the label now reads '10x real time
    (slowed for totality)' instead of just '10x real time'.

--- src/SolarEclipse2026.vue
+++ src/SolarEclipse2026.vue
@@ -1434,7 +1434,7 @@
                 </div>
             </div>
             <div id="speed-text">
-              Speed: {{ niceRound(playbackRate) }}x real time<span v-if="!playing"> (paused)</span>
+              Speed: {{ niceRound(playbackRate) }}x real time<span v-if="!playing"> (paused)</span><span v-else-if="forceRate"> (slowed for totality)</span>
             </div>
           </div>
           <div id="slider">
